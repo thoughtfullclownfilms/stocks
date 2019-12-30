55704fc26112eac6333738f1fc6864523aff12cd
Neeraj <sinne07@gmail.com>
2019-12-30 22:31:24 +0530

    Updates

---
+++ precache-manifest.0e4a149f0b7983733bed0b14383f8dfe.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "69b62e13ae8848d78dfc07db069842f1",
+    "url": "/stocks/index.html"
+  },
+  {
+    "revision": "6cde4ed411c520023d3d",
+    "url": "/stocks/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "7909e0a3a6e1a5b9e597",
+    "url": "/stocks/static/js/2.cdbc335e.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/stocks/static/js/2.cdbc335e.chunk.js.LICENSE"
+  },
+  {
+    "revision": "6cde4ed411c520023d3d",
+    "url": "/stocks/static/js/main.382cc6c8.chunk.js"
+  },
+  {
+    "revision": "8e26b66e475cda2fa305",
+    "url": "/stocks/static/js/runtime-main.3cc8945d.js"
+  }
+]);
